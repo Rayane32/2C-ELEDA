807e33ff1ccda75c623b43fecfe73620aa30b466
Rayane32 <rayaneescola32@gmail.com>
2019-05-13 19:05:04 -0300

    Coração Quebrado - 45%

---
+++ Assets/Life.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Life : MonoBehaviour
+{
+
+    public int life;
+    public GameObject[] spriteHearts;
+
+    void Start() {
+        StartCoroutine(LowerUpdate());    
+    }
+
+    public void SetHearts(int curLife){
+        for(int i = 0; i < spriteHearts.Length; i++){
+            if(i > (curLife - 1)){
+                Animator anim = spriteHearts[i].GetComponent<Animator>();
+                anim.SetTrigger("Broken");
+            }
+        }
+    }
+
+    IEnumerator LowerUpdate(){
+        for(;;){
+            SetHearts(life);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
